2f386ee86f79c4bc04e47f1630cee0c927265c1f
shumani <shumanisimeli@gmail.com>
2024-10-16 12:45:55 +0200

    created db.js file

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const uri = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new MongoClient(uri, { useUnifiedTopology: true });
+    this.client.connect((err) => {
+      if (err) {
+        console.error('MongoDB connection error:', err);
+      } else {
+        this.db = this.client.db(database);
+      }
+    });
+  }
+
+  isAlive() {
+    return !!this.client && !!this.client.topology && this.client.topology.isConnected();
+  }
+
+  async nbUsers() {
+    if (!this.db) return 0;
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    if (!this.db) return 0;
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
